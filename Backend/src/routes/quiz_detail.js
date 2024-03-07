2675407139ecc5b79800652d6b1f0357a5a0e306
roger-niyomugabo <rogerniyomugabo3@gmail.com>
2024-03-07 12:03:41 +0200

    implement backend functionality for deleting a quiz

--- Backend/src/routes/quiz_detail.js
+++ Backend/src/routes/quiz_detail.js
@@ -1,11 +1,22 @@
-/* eslint-disable no-unused-vars */
 import express from 'express';
+import output from '../utils/response';
+import { Quiz } from '../db/models/Quiz.model';
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 // function to delete a quiz
-router.delete('/delete', async (req, res) => {
-
+router.delete('/', async (req, res) => {
+  try {
+    const { quizId } = req.params;
+    const quizExists = await Quiz.findOne({ where: { id: quizId } });
+    if (!quizExists) {
+      return output(res, 400, 'Quiz does not exist', null, 'BAD_REQUEST');
+    }
+    await quizExists.destroy();
+    return output(res, 200, 'Quiz deleted successfully', null, null);
+  } catch (error) {
+    return output(res, 500, error.message || error, null, 'SERVER_ERROR');
+  }
 });
 
 // function to update a quiz
